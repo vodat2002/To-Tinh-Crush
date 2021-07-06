103960af433458b3bf18e673e783e9ea4ed5fdcf
vodat2002 <86997809+vodat2002@users.noreply.github.com>
2021-07-06 00:15:37 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Yêu anh Nguyễn Văn Hoài bảo`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Phải chăng em đã yêu anh Bảo từ cái nhìn đầu tiên😙',
+    desc: 'hahahahahahahahahahahahahaha ',
+    btnYes: 'Em đã yêu anh rồi chứ gì',
+    btnNo: 'Nono, ghê quá đi :3',
+    question:'Võ Thành Đạt',
+    btnReply: 'Gửi cho anh Bảo <3',
+    reply: 'Mày có yêu tao không thì bảo <33333333',
+    mess: 'Yêu chứ 🥰. Yêu anh Bảo nhiều nhiều 😘😘',
+    messDesc: 'Yêu tao thì kệ mày okkkkkkkk',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
